818332d9097833ebc1100658118cc8641a8360d0
Yunier Alvarez <yunieralvarezdev@iclould.com>
2019-02-28 12:33:12 -0500

    Add hasing playground file

--- server/server.js
+++ server/server.js
@@ -11,11 +11,11 @@ var {User} = require('./models/user');
 
 
 var app = express();
-var port = process.env.PORT;
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.post('/todos/', (req, res) => {
+app.post('/todos', (req, res) => {
   console.log(req.body.text);
 
   var todo = new Todo({
@@ -73,30 +73,25 @@ app.delete('/todos/:id', (req, res) => {
   }) ;
 });
 
+// TODO: test this, usermodel - 15
+app.post('/users', (req, res) => {
+  var body _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then((user) => {
+    res.send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
 app.patch('/todos/:id', (req, res) => {
   var id = req.params.id;
-  var body = _.pick(req.body, ['text', 'completed']);
+  var body = _.pick(req.body, ['text']);
 
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
-
-  if (_.isBoolean(body.completed) && body.completed) {
-    body.completedAt = new Date().getTime();
-  } else {
-    body.completed = false;
-    body.completedAt = null;
-  }
-
-  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
-    if (!todo) {
-      return res.status(404).send();
-    }
-
-    res.send({todo});
-  }).catch((e) => {
-    res.status(400).send();
-  })
 });
 
 app.listen(port, () => {
